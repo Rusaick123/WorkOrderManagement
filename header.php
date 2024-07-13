2d5ea269ec686e6bfce1762903bf2f7203e1e69c
Rusaick <121766688+Rusaick123@users.noreply.github.com>
2024-07-13 22:46:34 +0530

    updated

--- header.php
+++ header.php
@@ -3,4 +3,8 @@
         <img src="img/logo.jpeg" alt="Logo">
         <h1>Work Order Management System</h1>
     </div>
+    <ul>
+    <span class="user-id">User ID: 12345</span>
+        <li><a href="login.php">Logout</a></li>
+    </ul>   
 </nav>
--- user_register_form.php
+++ user_register_form.php
@@ -8,39 +8,45 @@
 </head>
 <body>
 <?php include 'header.php'; ?>
-<div class="register-container">
-    <form class="register-form" action="register_check.php" method="post">
-        <h2>Register</h2>
-        <div class="form-group">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
-        </div>
-        <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <div class="form-group">
-            <label for="emailid">Email ID:</label>
-            <input type="email" id="emailid" name="emailid" required>
-        </div>
-        <div class="form-group">
-            <label for="usertype">User Type:</label>
-            <select id="usertype" name="usertype" required>
-                <option value="user">User</option>
-                <option value="recommender">Recommender</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="department">Department:</label>
-            <select name="department" id="department" required>
-                <option value=" Office of the Dean"> Office of the Dean</option>
-                <option value="Dept. of Languages">Dept. of Languages</option>
-                <option value="Dept. of political Sciences">Dept. of political Sciences</option>
-            </select>
-        </div>
-        <input type="submit" value="Register">
-    </form>
+<div class="content">
+    <div class="login-container">
+        <form class="login-form" action="register_check.php" method="post">
+            <h2>User Registration</h2>
+            <div class="form-group">
+                <label for="userid">User Id:</label>
+                <input type="text" id="userid" name="userid" required>
+            </div>
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="usertype">User Type:</label>
+                <select id="usertype" name="usertype" required>
+                    <option value="user">User</option>
+                    <option value="admin">Admin</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <label for="department">Department:</label>
+                <select id="department" name="department" required>
+                    <option value="Office of the Dean">Office of the Dean</option>
+                    <option value="Dept. of Languages">Dept. of Languages</option>
+                    <option value="Dept. of Political Sciences">Dept. of Political Sciences</option>
+                </select>
+            </div>
+            <button type="submit">Register</button>
+        </form>
+    </div>
 </div>
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
 </body>
 </html>
